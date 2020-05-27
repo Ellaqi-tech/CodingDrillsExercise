dc3f000d95493322605dd335e9e059a4c599fac2
Ellaqi <ellaqi77777@gmail.com>
2020-05-27 17:05:06 -0400

    update J3

--- 2020/J2020.js
+++ 2020/J2020.js
@@ -1,34 +1,27 @@
-const load = () => {
-    //Problem J1: Dog Treats
-    const formhanlde1 = document.forms[0];       
-    const onsubmit1 = () => {
-        const S = document.getElementById("s").value;
-        const M = document.getElementById("m").value;
-        const L = document.getElementById("l").value;
-        const output1 = document.getElementById("output1");
-        
-        const operation1 = (s,m,l) => {
-            //positive input validation
-            if (s>10 || s<0 || m>10 || m<0 || l>10 || l<0) {
-                output1.innerHTML = "please insert an interger between 0 and 10"; 
-                console.log(s,m,l);
-                return false;
-                } 
-            //interger input validation
-            if(!(Number.isInteger(Number(s)) && Number.isInteger(Number(m)) && Number.isInteger(Number(l)))) {
-                output1.innerHTML = "please insert an interger between 0 and 10"; 
-                return false;
-                }
-            output1.innerHTML =(1*s + 2*m + 3*l) >= 10 ? "Barley is Happy." : "Barley is Sad.";
+//Problem J1: Dog Treats 
+const j1function = () => {      
+    const S = document.getElementById("s").value;
+    const M = document.getElementById("m").value;
+    const L = document.getElementById("l").value;
+    const output1 = document.getElementById("output1");
+
+    //positive input validation
+    if (S>10 || S<0 || M>10 || M<0 || L>10 || L<0) {
+        output1.innerHTML = "please insert an interger between 0 and 10"; 
+        console.log(S,M,L);
+        } 
+    //interger input validation
+    else if(!(Number.isInteger(Number(S)) && Number.isInteger(Number(M)) && Number.isInteger(Number(L)))) {
+        output1.innerHTML = "please insert an interger between 0 and 10"; 
+        } else {
+            output1.innerHTML =(1*S + 2*M + 3*L) >= 10 ? "Barley is Happy." : "Barley is Sad.";
         }
-        operation1(S,M,L);
-        return false;
-    }        
-    formhanlde1.onsubmit = onsubmit1;
+}
+
     
-    //Problem J2: Epidemiology
-    const formhanlde2 = document.forms[1];       
-    const onsubmit2 = () => {
+//Problem J2: Epidemiology
+const j2function = () => {      
+    //const onsubmit2 = () => {
         const Pvalue = document.getElementById("P").value;
         const Nvalue = document.getElementById("N").value;
         const Rvalue = document.getElementById("R").value;
@@ -39,12 +32,10 @@ const load = () => {
             if (P>Math.pow(10, 7) || P<0 || N<0 || R>10 || R<0) {
                 output2.innerHTML = "please insert P ≤ 10 to the 7th power, N ≤ P, R ≤ 10"; 
                 console.log(P,N,R);
-                return false;
                 } 
             //interger input validation
             if(!(Number.isInteger(Number(P)) && Number.isInteger(Number(N)) && Number.isInteger(Number(R)))) {
                 output2.innerHTML = "please insert intergers"; 
-                return false;
                 }
             //logic
             let p = parseInt(Pvalue);
@@ -59,47 +50,47 @@ const load = () => {
             }
             output2.innerHTML = `Over ${p} people have disease by the end of Day ${day}.`;
         }
-        operation2(Pvalue,Nvalue,Rvalue);
-        return false;
-    }        
-    formhanlde2.onsubmit = onsubmit2;
-    
-    //Problem J3: Art
-    const formhanlde3 = document.forms[2];  
-    //NaN?
-    const dropsValue = document.getElementById("drops").value;
-    const drops = parseInt(dropsValue);
-    
-    const coordinatesBtn = document.getElementById("coordinates");
-    const container = document.getElementById("container");
-    const output3 = document.getElementById("output3");
-
-    const outputContainer = () => {
-        alert(dropsValue);
-        for (let i = 1; i <= drops; i++) {
-            container.innerHTML += 
-                `<div id="coordinate">
-                    <span>
-                        <label for="X${i}"> X </label>
-                        <input type="text" name="X${i}" id="X${i}">
-                    </span>
-                    <span>
-                        <label for="Y${i}"> Y </label>
-                        <input type="text" name="Y${i}" id="Y${i}">
-                    </span>
-                </div>`;
-            console.log(i, container.innerHTML);
-        }
-    }
-    coordinatesBtn.addEventListener("click",outputContainer);
-    const onsubmit3 = () => {
-        var xArray = [];
-        var yArray = [];
-        xArray = document.getElementById(`X${i}`).push();
-        yArray = document.getElementById(`Y${i}`).push();
-        output3.innerHTML = ` ${Math.max(xArray+1)}, ${Math.max(yArray+1)}`;
-        return false;
-    }        
-    formhanlde3.onsubmit = onsubmit3;  
+        operation2(Pvalue,Nvalue,Rvalue);       
 }
-window.onload = load;
+    
+//Problem J3: Art
+const j3function = () => {
+    const container = document.getElementById("container");
+    const btn3 = document.getElementById("btn3");
+    const output3 = document.getElementById("output3");
+    const dropsValue = document.getElementById("drops").value;
+
+    var xArray = [];
+    var yArray = [];
+
+    for (let i = 1; i <= dropsValue; i++) {
+        container.innerHTML += 
+            `
+            <div id="coordinate">
+                <span>
+                    <label for="X${i}"> X </label>
+                    <input type="text" name="X${i}" id="X${i}">
+                </span>
+                <span>
+                    <label for="Y${i}"> Y </label>
+                    <input type="text" name="Y${i}" id="Y${i}">
+                </span>
+            </div>
+            `;
+    }
+
+    //add event listener because output() is inside another function
+    //console.log(btn3); can get btn3 element
+    //but no alert, seems it couldn't access to the output function after click the button
+    btn3.addEventListener = ("click", output = () => {
+        alert("output");
+        for (let j = 1; j <= dropsValue; j++) {
+            xArray = document.getElementById(`X${j}`).value.push;
+            yArray = document.getElementById(`Y${j}`).value.push;
+            console.log("x "+xArray+"y "+yArray);
+        } 
+        output3.innerHTML = ` ${Math.max(xArray)+1}, ${Math.max(yArray)+1}`;
+    })
+}
+    
+
